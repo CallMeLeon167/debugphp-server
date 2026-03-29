6998e03cc5bc7bb96c6e93fdae2daff93efea071
Leon Schmidt <leon18058r@gmail.com>
2026-03-29 20:31:31 +0200

    feat: Add license information and repository link to dashboard.php header

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of the DebugPHP Server.
+ *
+ * (c) Leon Schmidt <kontakt@callmeleon.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/CallMeLeon167/debugphp-server
+ */
+
 use DebugPHP\Server\Config;
 ?>
 
