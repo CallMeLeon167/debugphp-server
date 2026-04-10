fb881ddb5ee00f627a5176e4c6916fbd0efe12e9
Leon Schmidt <leon18058r@gmail.com>
2026-04-10 16:59:14 +0200

    fix: Update version number to 0.1.1 in Config class

--- src/Config.php
+++ src/Config.php
@@ -29,7 +29,7 @@ namespace DebugPHP\Server;
 final class Config
 {
     /** @var string */
-    private string $version = '0.1.0';
+    private string $version = '0.1.1';
 
     /** @var self */
     private static self $instance;
